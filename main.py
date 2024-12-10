60efd5d6f0cae1033e304d2e85f4b27b17809234
alvecap <alvecapital60@gmail.com>
2024-12-10 09:44:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import os
-from betting_bot import main as bot_main
 import threading
 
 app = Flask(__name__)
@@ -9,14 +8,6 @@ app = Flask(__name__)
 def health_check():
     return 'OK', 200
 
-def run_bot():
-    bot_main()
-
 if __name__ == "__main__":
-    # Démarrer le bot dans un thread séparé
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-    
-    # Démarrer le serveur Flask
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
